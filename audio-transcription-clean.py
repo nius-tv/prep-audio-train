81ad7ecc62bbb5356bcdea0e8ba6dbd1bff6aa1d
Carlos Chinchilla <calufa@gmail.com>
2019-08-05 22:42:29 -0700

    add audio-transcription-clean step

---
+++ audio-transcription-clean.py
@@ -0,0 +1,24 @@
+import json
+import spacy
+
+
+with open('data/audio.transcription.flat.txt') as f:
+	data = f.read()
+data = data.lower().replace('’', '\'')
+
+nlp = spacy.load('en_core_web_lg')
+words = []
+for line in data.split('\n'):
+	if line.strip() == '':
+		continue
+	for word in json.loads(line):
+		text = word['word']
+		for token in nlp(text):
+			if token.pos_ in ['PUNCT', 'SPACE']:
+				continue
+			words.append(token.text)
+
+print(len(words))
+words = ' '.join(words)
+with open('data/audio.transcription.clean.txt', 'w') as f:
+	f.write(words)
