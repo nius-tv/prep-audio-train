6e5a0490493e0e38c5365082ae9e4dc25a7a60c6
Carlos Chinchilla <calufa@gmail.com>
2019-08-11 20:22:09 -0700

    add flat-audio-transcriptions step

---
+++ flat-audio-transcriptions.py
@@ -0,0 +1,30 @@
+import json
+
+
+if __name__ == '__main__':
+    files = [
+        'data/0-5hours.transcription.json',
+        'data/5-10hours.transcription.json',
+        'data/10-15hours.transcription.json',
+        'data/15-20hours.transcription.json',
+        'data/20-25hours.transcription.json'
+    ]
+    output_file = open('data/audio-transcription.flat.txt', 'w')
+
+    for i, filename in enumerate(files):
+        with open(filename) as f:
+            data = f.read()
+        data = json.loads(data)
+
+        for result in data['response']['results']:
+            for word in result['alternatives'][0]['words']:
+                start = word['startTime'][0:-1]
+                start = float(start) + (i * 60 * 60 * 5)
+                word['startTime'] = start
+
+                end = word['endTime'][0:-1]
+                end = float(end) + (i * 60 * 60 * 5)
+                word['endTime'] = end
+
+                line = json.dumps(word)
+                output_file.write(line + '\n')
