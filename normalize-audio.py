1f239a4ecba5a50b4a7f23eca62bac8b97bd2b5f
Carlos Chinchilla <calufa@gmail.com>
2019-08-26 14:47:57 -0700

    fix paths

--- normalize-audio.py
+++ normalize-audio.py
@@ -6,7 +6,7 @@ from utils import segment_audio
 if __name__ == '__main__':
     duration = 60
 
-    for input_audio in glob.glob('data/tmp/**'):
+    for input_audio in glob.glob('/data/silence/**'):
         file_name = input_audio.split('/')[-1]
-        output_audio = 'data/norm/{}'.format(file_name)
+        output_audio = '/data/norm/{}'.format(file_name)
         segment_audio(input_audio, output_audio, 0, duration)
