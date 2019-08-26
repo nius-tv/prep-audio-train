3784b1fcd47b1047f6ca563dfa016bb1a0c6258a
Carlos Chinchilla <calufa@gmail.com>
2019-08-26 14:47:14 -0700

    fix paths

--- add-silence.py
+++ add-silence.py
@@ -3,14 +3,14 @@ import subprocess
 
 
 def add_silence(input_audio, output_audio):
-    cmd = 'sox {i_file} data/silence.wav {o_file}'.format(i_file=input_audio,
-                                                          o_file=output_audio)
+    cmd = 'sox {i_file} /data/silence.wav {o_file}'.format(i_file=input_audio,
+                                                           o_file=output_audio)
     print(cmd)
     subprocess.call(['bash', '-c', cmd])
 
 
 if __name__ == '__main__':
-    for input_audio in glob.glob('data/segments/**'):
+    for input_audio in glob.glob('/data/segments/**'):
         file_name = input_audio.split('/')[-1]
-        output_audio = 'data/tmp/{}'.format(file_name)
+        output_audio = '/data/silence/{}'.format(file_name)
         add_silence(input_audio, output_audio)
