af760c219370b79079c9070de95df5a68aef6b65
Carlos Chinchilla <calufa@gmail.com>
2019-08-28 19:09:51 -0700

    fix style

--- flat-audio-transcriptions.py
+++ flat-audio-transcriptions.py
@@ -1,17 +1,15 @@
+import glob
 import json
 
 
 if __name__ == '__main__':
-    files = [
-        'data/0-5hours.transcription.json',
-        'data/5-10hours.transcription.json',
-        'data/10-15hours.transcription.json',
-        'data/15-20hours.transcription.json',
-        'data/20-25hours.transcription.json'
-    ]
-    output_file = open('data/audio-transcription.flat.txt', 'w')
+    max_seconds = 5 * 60 * 60 # 5 hours
+    files = sorted(glob.glob('/data/transcriptions/*'))
+    output_file = open('/data/audio-transcription.flat.txt', 'w')
 
     for i, file_path in enumerate(files):
+        print(file_path)
+
         with open(file_path) as f:
             data = f.read()
         data = json.loads(data)
@@ -19,11 +17,11 @@ if __name__ == '__main__':
         for result in data['response']['results']:
             for word in result['alternatives'][0]['words']:
                 start = word['startTime'][0:-1]
-                start = float(start) + (i * 60 * 60 * 5)
+                start = float(start) + (i * max_seconds)
                 word['startTime'] = start
 
                 end = word['endTime'][0:-1]
-                end = float(end) + (i * 60 * 60 * 5)
+                end = float(end) + (i * max_seconds)
                 word['endTime'] = end
 
                 line = json.dumps(word)
