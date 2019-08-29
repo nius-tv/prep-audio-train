dde66f92c1b6a5b69eb8eaaa418a0e82d7635eb3
Carlos Chinchilla <calufa@gmail.com>
2019-08-28 20:26:08 -0700

    move “duration” to config

--- normalize_audio.py
+++ normalize_audio.py
@@ -1,11 +1,10 @@
 import glob
 
+from config import duration
 from utils import segment_audio
 
 
 if __name__ == '__main__':
-    duration = 60
-
     for input_audio in glob.glob('/data/silence/**'):
         file_name = input_audio.split('/')[-1]
         output_audio = '/data/norm/{}'.format(file_name)
