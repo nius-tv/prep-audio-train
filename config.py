4f687e397d07f390a2168cc759ee2e90f192485c
Carlos Chinchilla <calufa@gmail.com>
2019-08-29 23:52:18 -0700

    add gcs_bucket and voice_name to config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+gcs_bucket = 'prep-audio'
+voice_name = 'LJSpeech-1.1'
 original_audio = '/data/original.wav'
 part_duration = 5 * 60 * 60 # 5 hours in seconds
 sample_rate = 16000 # 16 MHz audio sample rate
